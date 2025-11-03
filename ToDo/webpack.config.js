05b7a69f7260686bde3b9045f038aebdc701eea1
KudatoNado <kodinetsdaniil@gmail.com>
2025-11-03 19:37:42 +0300

    Start new commit

---
+++ ToDo/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require ('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.[fullhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module:{
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins:[
+  new HtmlWebpackPlugin({
+    title: 'Output Management',
+    template: path.resolve(__dirname, 'index.html') //__dirname=D:Tratsiak_Olga\Test - полный путь к шаблону
+  }),
+  new MiniCssExtractPlugin({
+    filename: 'index.[fullhash].css' //'[name].css - если написать так, то в dist будет присваиваться имя main.css
+  }),
+  ],
+};
